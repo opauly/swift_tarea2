568e09c76134ebd672e6247855f24c536ee98139
Oscar Pauly <OscarPauly@macbook-pro-de-oscar.local>
2016-03-18 23:55:10 -0600

    Initial Commit

---
+++ Swift_Tarea2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Tarea2
+//
+//  Created by Oscar Pauly on 18/3/16.
+//  Copyright © 2016 Oscar Pauly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
